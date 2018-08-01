1a409eb7b58d7e04a88e1f9618b9abc81b1ec466
HP ZBOOK 15 <HP ZBOOK 15@Pluviophile>
2018-08-01 09:33:16 +0700

    Coba IF

--- src/jobsheet7/CobaPercabangan.java
+++ src/jobsheet7/CobaPercabangan.java
@@ -5,6 +5,7 @@
  */
 package jobsheet7;
 
+import java.util.Scanner;
 /**
  *
  * @author HP ZBOOK 15
@@ -13,5 +14,22 @@ public class CobaPercabangan {
     public static void main(String[] args) {
         String identitas = "Maulana Hilmi Arkan / X RPL 5 / 21";
         System.out.println("Indentitas : " + identitas);
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. 1");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if(a == 5)
+            System.out.println("nilai a sama dengan 5");
+        
+        System.out.println("\nIF.. 3");
+        if(a > 5) {
+            System.out.println("isi variabel a : " + a);
+            System.out.println("nilai a lebih dari 5");
+        }
     }
 }
