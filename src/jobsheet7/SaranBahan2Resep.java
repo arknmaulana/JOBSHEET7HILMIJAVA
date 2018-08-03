ff2d46247c6a5b6049c45dc842e6bf11f20717fb
HP ZBOOK 15 <HP ZBOOK 15@Pluviophile>
2018-08-03 19:37:07 +0700

    Tambah Input Bahan 2

--- src/jobsheet7/SaranBahan2Resep.java
+++ src/jobsheet7/SaranBahan2Resep.java
@@ -5,10 +5,13 @@
  */
 package jobsheet7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author HP ZBOOK 15
  */
+
 public class SaranBahan2Resep {
     public static void main(String[] args) {
         String identitas = "Maulana Hilmi Arkan / X RPL 5 / 21";
@@ -23,5 +26,24 @@ public class SaranBahan2Resep {
         System.out.println("2. Telur");
         System.out.println("Masukkan no pilihan Anda :");
         int bahanl = scanner.nextInt();
+        
+        if(bahan1 == 1 || bahan1 == 2)
+        {
+            System.out.println("Bahan Kedua: ");
+            if(bahan1 == 1) {
+                System.outprintln("1. Susu");
+                System.outprintln("2. Minyak Goreng");
+                System.outprintln("3. Tidak ada");
+            }else{
+                System.outprintln("1. Minyak Goreng");
+                System.outprintln("2. Roti");
+                System.outprintln("3. Tidak ada");
+            }
+            System.out.print("Masukkan no pilihan Anda :");
+            int bahan2 = scanner.nextInt();
+            
+            
+        }else
+            System.out.println("Mohon maaf, pilihan tidak ditemukan, " + "tidak dapat memberikan saran resep");
     }
 }
