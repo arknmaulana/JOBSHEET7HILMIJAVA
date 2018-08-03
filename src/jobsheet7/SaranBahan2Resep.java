004469c8468f6da0959568abba9b774fa0e905ce
HP ZBOOK 15 <HP ZBOOK 15@Pluviophile>
2018-08-03 19:50:04 +0700

    Tambah Tampil Resep

--- src/jobsheet7/SaranBahan2Resep.java
+++ src/jobsheet7/SaranBahan2Resep.java
@@ -25,23 +25,45 @@ public class SaranBahan2Resep {
         System.out.println("1. Pisang");
         System.out.println("2. Telur");
         System.out.println("Masukkan no pilihan Anda :");
-        int bahanl = scanner.nextInt();
+        int bahan1 = scanner.nextInt();
         
         if(bahan1 == 1 || bahan1 == 2)
         {
             System.out.println("Bahan Kedua: ");
             if(bahan1 == 1) {
-                System.outprintln("1. Susu");
-                System.outprintln("2. Minyak Goreng");
-                System.outprintln("3. Tidak ada");
+                System.out.println("1. Susu");
+                System.out.println("2. Minyak Goreng");
+                System.out.println("3. Tidak ada");
             }else{
-                System.outprintln("1. Minyak Goreng");
-                System.outprintln("2. Roti");
-                System.outprintln("3. Tidak ada");
+                System.out.println("1. Minyak Goreng");
+                System.out.println("2. Roti");
+                System.out.println("3. Tidak ada");
             }
             System.out.print("Masukkan no pilihan Anda :");
             int bahan2 = scanner.nextInt();
             
+            if(bahan2 >= 1 && bahan2 <= 3)
+            {
+                if(bahan1==1)
+                    switch(bahan2)
+                    {
+                        case 1: System.out.println(
+                                "Anda dapat membuat milk shake banana"); break;
+                        case 2: System.out.println(
+                                "Anda dapat membuat pisang goreng"); break;
+                        case 3: System.out.println(
+                                "Anda dapat membuat pisang rebus");
+                    }
+                else
+                    switch(bahan2)
+                    {
+                        case 1: System.out.println(
+                                "Anda dapat membuat telur mata sapi"); break;
+                        case 2: System.out.println(
+                                "Anda dapat membuat sandwich telur"); break;
+                        case 3: System.out.println(
+                                "Anda dapat membuat telur rebus"); break;
+                    }
             
         }else
             System.out.println("Mohon maaf, pilihan tidak ditemukan, " + "tidak dapat memberikan saran resep");
